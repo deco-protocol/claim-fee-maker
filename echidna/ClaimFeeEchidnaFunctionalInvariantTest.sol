427fc3eed74a9c44f5c936bcf36e27326db16db3
Balaji Boggaram Ramanarayan <bbalaji@uber.com>
2022-06-12 16:55:35 -0700

    Implement fuzz tests for rewind, withdraw, activate, slice, merge

--- echidna/ClaimFeeEchidnaFunctionalInvariantTest.sol
+++ echidna/ClaimFeeEchidnaFunctionalInvariantTest.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 
 import {ClaimFee} from "../src/ClaimFee.sol";
 import {Gate1} from "./deps/gate1.sol";
-import "./deps/vat.sol"; 
+import "./deps/vat.sol";
 import "./deps/DSMath.sol";
 import "./deps/Vm.sol";
 
@@ -17,7 +17,7 @@ import "./TestClaimHolder.sol";
 import "./TestUtil.sol";
 
 contract ClaimFeeEchidnaFunctionalInvariantTest is DSMath {
-    
+
     Vm public vm = Vm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
 
     uint256 t0;
@@ -62,12 +62,12 @@ contract ClaimFeeEchidnaFunctionalInvariantTest is DSMath {
         vow = new MockVow(address(vat));
         gate = new Gate1(address(vow));
         vat.rely(address(gate));
-        gate.file("approvedtotal", testUtil.rad(10000)); // set draw limit 
+        gate.file("approvedtotal", testUtil.rad(10000)); // set draw limit
 
         // claimfee
         cfm = new ClaimFee(address(gate));
         gov_user = new GovernanceUser(cfm);
-        gov_addr = address(gov_user);  
+        gov_addr = address(gov_user);
         cfm.rely(gov_addr); // Add gov user as a ward.
         gate.kiss(address(cfm)); // Add a CFM as integration to gate
 
@@ -82,7 +82,7 @@ contract ClaimFeeEchidnaFunctionalInvariantTest is DSMath {
         // ILK : ETH_A
         vat.ilkSetup(ETH_A); // vat initializes ILK (ETH_A).
         gov_user.initializeIlk(ETH_A); // Gov initializes ILK (ETH_A) in Claimfee as gov is a ward
-        
+
         // ILK : WBTC_A
         vat.ilkSetup(WBTC_A); // Vat initializes ILK (WBTC_A)
         gov_user.initializeIlk(WBTC_A); // gov initialize ILK (WBTC_A) in claimfee as gov is a ward
@@ -94,12 +94,12 @@ contract ClaimFeeEchidnaFunctionalInvariantTest is DSMath {
         vat.increaseRate(WBTC_A, testUtil.wad(5), address(vow));
         cfm.snapshot(WBTC_A); // take a snapshot at t0 @ 1.05
         cfm.issue(WBTC_A, usr_addr, t0, t2, testUtil.wad(5000)); // issue cf 5000 to cHolder
-        
+
     }
 
     // Fuzz Goal : User transfers claim fee to another user.  The balances are adjusted accordingly.
     function test_moveclaim(address src, address dest, bytes32 class_, uint256 bal) public  {
-        
+
         uint256 srcBalance = cfm.cBal(src,class_);
         uint256 destBalance = cfm.cBal(dest, class_);
 
@@ -110,7 +110,7 @@ contract ClaimFeeEchidnaFunctionalInvariantTest is DSMath {
             assert(
                 msg.sender != src && testUtil.cmpStr(errMessage, "not-allowed") ||
                 cfm.can(msg.sender, src) != 1 && testUtil.cmpStr(errMessage, "not-allowed") ||
-                cfm.cBal(src, class_) < bal 
+                cfm.cBal(src, class_) < bal
             );
         }
     }
@@ -128,7 +128,7 @@ contract ClaimFeeEchidnaFunctionalInvariantTest is DSMath {
 
         } catch Error(string memory error_message) {
             assert(
-                cfm.latestRateTimestamp(ETH_A) <= iss  && testUtil.cmpStr(error_message, "timestamp/invalid") || 
+                cfm.latestRateTimestamp(ETH_A) <= iss  && testUtil.cmpStr(error_message, "timestamp/invalid") ||
                 block.timestamp > mat && testUtil.cmpStr(error_message,"timestamp/invalid") ||
                 cfm.rate(ETH_A, iss) == 0 && testUtil.cmpStr(error_message, "rate/invalid")
             );
@@ -138,7 +138,7 @@ contract ClaimFeeEchidnaFunctionalInvariantTest is DSMath {
     }
 
     function test_initialize(bytes32 ilk) public {
-        
+
         // Initialize the ilk in VAT
         vat.ilkSetup(ilk);
         vat.increaseRate(ilk, testUtil.wad(5), address(vow));
@@ -162,8 +162,10 @@ contract ClaimFeeEchidnaFunctionalInvariantTest is DSMath {
         vat.ilkSetup(ilk);
         vat.increaseRate(ilk, testUtil.wad(10), address(vow));
         try cfm.snapshot(ilk) {
+
             assert(cfm.rate(ilk,block.timestamp) == testUtil.wad(10));
             assert(cfm.latestRateTimestamp(ilk) == block.timestamp);
+
         } catch Error(string memory error_message) {
             assert(
                 cfm.initializedIlks(ilk) == false && testUtil.cmpStr(error_message, "ilk/not-initialized")
@@ -181,7 +183,9 @@ contract ClaimFeeEchidnaFunctionalInvariantTest is DSMath {
 
         // cfm.rely(sender, usr); // this can be removed
          try cfm.insert(ilk, tBefore, tInsert, rate) {
+
                 assert(cfm.rate(ilk, tInsert) == rate);
+
          } catch Error(string memory error_message) {
              assert(
                  (tBefore >= tInsert)  && testUtil.cmpStr(error_message, "rate/timestamps-not-in-order") ||
@@ -197,29 +201,32 @@ contract ClaimFeeEchidnaFunctionalInvariantTest is DSMath {
          }
      }
 
-    function test_collect(bytes32 ilk, address usr, uint256 issTS, uint256 matTS, uint256 collectTS, uint256 bal) public {
+    function test_collect(bytes32 ilk, address usr_address, uint256 issTS, uint256 matTS, uint256 collectTS, uint256 bal) public {
+
          vat.ilkSetup(ilk);
          vat.increaseRate(ilk, testUtil.wad(10), address(vow));
          cfm.initializeIlk(ilk);
-         // issue ?? 
+         // issue ??
 
         uint256 issRate = cfm.rate(ilk, issTS);
         uint256 collectRate = cfm.rate(ilk, collectTS);
 
         bytes32 ilkClassIssMat = keccak256(abi.encodePacked(ilk, issTS, matTS)); // iss, mat
         bytes32 ilkClassColMat = keccak256(abi.encodePacked(ilk, collectTS, matTS)); // collect, mat
-        uint256 issBalance = cfm.cBal(usr, ilkClassIssMat);
-        uint256 collectBalance = cfm.cBal(usr, ilkClassColMat);
+        uint256 issBalance = cfm.cBal(usr_address, ilkClassIssMat);
+        uint256 collectBalance = cfm.cBal(usr_address, ilkClassColMat);
 
-        try cfm.collect(ilk, usr, issTS, matTS, collectTS, bal) {
-            assert(cfm.cBal(usr, ilkClassIssMat) == issBalance - bal);
+        try cfm.collect(ilk, usr_address, issTS, matTS, collectTS, bal) {
+
+            assert(cfm.cBal(usr_address, ilkClassIssMat) == issBalance - bal);
             if (collectTS != matTS) {
-                assert(cfm.cBal(usr, ilkClassColMat) == collectBalance + bal);
+                assert(cfm.cBal(usr_address, ilkClassColMat) == collectBalance + bal);
             }
+
         } catch Error(string memory error_message) {
             assert(
-                msg.sender != usr && testUtil.cmpStr(error_message, "not-allowed") ||
-                cfm.can(msg.sender, usr) != 1 && testUtil.cmpStr(error_message, "not-allowed") ||
+                msg.sender != usr_address && testUtil.cmpStr(error_message, "not-allowed") ||
+                cfm.can(msg.sender, usr_address) != 1 && testUtil.cmpStr(error_message, "not-allowed") ||
                 issRate == 0 && testUtil.cmpStr(error_message, "rate/invalid") ||
                 collectRate == 0 && testUtil.cmpStr(error_message, "rate/invalid") ||
                 issTS > collectTS && testUtil.cmpStr(error_message, "timestamp/invalid") ||
@@ -230,9 +237,144 @@ contract ClaimFeeEchidnaFunctionalInvariantTest is DSMath {
         }
     }
 
-    function rely(address usr) public {
-        try cfm.rely(usr) {
-            assert(cfm.wards(usr) == 1);
+    function  test_slice(bytes32 ilk, address usr_address, uint256 ts1, uint256 ts2, uint256 ts3, uint256 bal) public {
+
+        bytes32 ilkClass_t1_t3 = keccak256(abi.encodePacked(ilk, ts1, ts3)); // t1, t3
+        bytes32 ilkClass_t1_t2 = keccak256(abi.encodePacked(ilk, ts1, ts2)); // t1, t2
+        bytes32 ilkClass_t2_t3 = keccak256(abi.encodePacked(ilk, ts2, ts3)); // t2, t3
+
+        uint256  prevBalance_t1_t3 =  cfm.cBal(usr_address, ilkClass_t1_t3);
+        uint256  prevBalance_t1_t2 =  cfm.cBal(usr_address, ilkClass_t1_t2);
+        uint256  prevBalance_t2_t3 =  cfm.cBal(usr_address, ilkClass_t2_t3);
+
+
+        try cfm.slice(ilk, usr_address, t1, t2, t3, bal) {
+
+            assert(cfm.cBal(usr_address, ilkClass_t1_t3) == prevBalance_t1_t3 - bal);
+            assert(cfm.cBal(usr_address, ilkClass_t1_t2) == prevBalance_t1_t2 + bal);
+            assert(cfm.cBal(usr_address, ilkClass_t2_t3) == prevBalance_t2_t3 + bal);
+
+        } catch Error (string memory error_message) {
+            assert(
+                msg.sender != usr_address && testUtil.cmpStr(error_message, "not-allowed") ||
+                cfm.can(msg.sender, usr_address) != 1 && testUtil.cmpStr(error_message, "not-allowed") ||
+                t1 >= t2 && testUtil.cmpStr(error_message, "timestamp/invalid") ||
+                t2 >= t3 && testUtil.cmpStr(error_message, "timestamp/invalid") ||
+                cfm.cBal(usr_address, ilkClass_t1_t3) < bal && testUtil.cmpStr(error_message, "cBal/insufficient-balance")
+            );
+        } catch {
+            assert(false);
+        }
+    }
+
+    function test_merge(bytes32 ilk, address usr_address, uint256 ts1, uint256 ts2, uint256 ts3, uint256 bal) public {
+
+        bytes32 ilkClass_t1_t3 = keccak256(abi.encodePacked(ilk, ts1, ts3)); // t1, t3
+        bytes32 ilkClass_t1_t2 = keccak256(abi.encodePacked(ilk, ts1, ts2)); // t1, t2
+        bytes32 ilkClass_t2_t3 = keccak256(abi.encodePacked(ilk, ts2, ts3)); // t2, t3
+
+        uint256  prevBalance_t1_t3 =  cfm.cBal(usr_address, ilkClass_t1_t3);
+        uint256  prevBalance_t1_t2 =  cfm.cBal(usr_address, ilkClass_t1_t2);
+        uint256  prevBalance_t2_t3 =  cfm.cBal(usr_address, ilkClass_t2_t3);
+
+        try cfm.merge(ilk, usr_address, ts1, ts2, ts3, bal) {
+
+            assert(cfm.cBal(usr_address, ilkClass_t1_t3) == prevBalance_t1_t3 + bal);
+            assert(cfm.cBal(usr_address, ilkClass_t1_t2) == prevBalance_t1_t2 - bal);
+            assert(cfm.cBal(usr_address, ilkClass_t2_t3) == prevBalance_t2_t3 - bal);
+
+        } catch Error (string memory error_message) {
+            assert(
+                msg.sender != usr_address && testUtil.cmpStr(error_message, "not-allowed") ||
+                cfm.can(msg.sender, usr_address) != 1 && testUtil.cmpStr(error_message, "not-allowed") ||
+                t1 >= t2 && testUtil.cmpStr(error_message, "timestamp/invalid") ||
+                t2 >= t3 && testUtil.cmpStr(error_message, "timestamp/invalid") ||
+                (t1 >= t2 || t2 >= t3) && testUtil.cmpStr(error_message, "timestamp/invalid") ||
+                cfm.cBal(usr_address, ilkClass_t1_t2) < bal && testUtil.cmpStr(error_message, "cBal/insufficient-balance") ||
+                cfm.cBal(usr_address, ilkClass_t2_t3) < bal && testUtil.cmpStr(error_message, "cBal/insufficient-balance")
+            );
+        }
+
+    }
+
+    function test_rewind(bytes32 ilk, address usr_address, uint256 issuanceTs, uint256 maturityTs, uint256 rewindTs, uint256 bal) public {
+
+        uint256 rewindRate = cfm.rate(ilk, rewindTs);
+        uint256 issuanceRate = cfm.rate(ilk, issuanceTs);
+
+        bytes32 ilkClass_iss_mat = keccak256(abi.encodePacked(ilk, issuanceTs, maturityTs)); // iss, mat
+        bytes32 ilkClass_rew_mat = keccak256(abi.encodePacked(ilk, rewindTs, maturityTs)); // rewind, mat
+
+        uint256  prevBalance_iss_mat =  cfm.cBal(usr_address, ilkClass_iss_mat);
+        uint256  prevBalance_rew_mat =  cfm.cBal(usr_address, ilkClass_rew_mat);
+
+
+        try cfm.rewind(ilk, usr_address, issuanceTs, maturityTs, rewindTs, bal) {
+
+            assert(cfm.cBal(usr_address, ilkClass_iss_mat) == prevBalance_iss_mat - bal);
+            assert(cfm.cBal(usr_address, ilkClass_rew_mat) == prevBalance_rew_mat + bal);
+
+        } catch Error(string memory error_message) {
+            assert(
+                msg.sender != usr_address && testUtil.cmpStr(error_message, "not-allowed") ||
+                cfm.can(msg.sender, usr_address) != 1 && testUtil.cmpStr(error_message, "not-allowed") ||
+                rewindTs > issuanceTs && testUtil.cmpStr(error_message, "timestamp/invalid") ||
+                issuanceTs > maturityTs && testUtil.cmpStr(error_message, "timestamp/invalid") ||
+                rewindRate == 0 && testUtil.cmpStr(error_message, "rate/invalid") ||
+                issuanceRate == 0 && testUtil.cmpStr(error_message, "rate/invalid") ||
+                issuanceRate <= rewindRate && testUtil.cmpStr(error_message, "rate/no-difference")
+            );
+        } catch {
+            assert(false);
+        }
+    }
+
+    function test_activate(bytes32 ilk, address user_address, uint256 ts1, uint256 ts2, uint256 ts3, uint256 bal) public {
+
+        bytes32 ilkClass_t1_t2 = keccak256(abi.encodePacked(ilk, ts1, ts2)); // t1, t2
+        bytes32 ilkClass_t2_t3 = keccak256(abi.encodePacked(ilk, ts2, ts3)); // t2, t3
+
+        uint256  prevBalance_t1_t2 =  cfm.cBal(user_address, ilkClass_t1_t2);
+        uint256  prevBalance_t2_t3 =  cfm.cBal(user_address, ilkClass_t2_t3);
+
+        try cfm.activate(ilk, user_address, ts1, ts2, ts3, bal) {
+            assert(cfm.cBal(user_address, ilkClass_t1_t2) == prevBalance_t1_t2 - bal);
+            assert(cfm.cBal(user_address, ilkClass_t2_t3) == prevBalance_t2_t3 + bal);
+
+        } catch Error (string memory error_message) {
+            assert(
+                ts1 >= ts2 && testUtil.cmpStr(error_message, "timestamp/invalid") ||
+                ts2 >= ts3 && testUtil.cmpStr(error_message, "timestamp/invalid") ||
+                msg.sender != user_address && testUtil.cmpStr(error_message, "not-allowed") ||
+                cfm.can(msg.sender, user_address) != 1 && testUtil.cmpStr(error_message, "not-allowed") ||
+                cfm.rate(ilk, ts1) !=0 && testUtil.cmpStr(error_message, "rate/invalid") ||
+                cfm.rate(ilk, ts2) == 0 && testUtil.cmpStr(error_message, "timestamp/invalid")
+            );
+        } catch {
+            assert(false);
+        }
+    }
+
+    function test_withdraw(bytes32 ilk, address user_address, uint256 issuanceTs, uint256 maturityTs, uint256 bal) public {
+
+        bytes32 ilkClass_iss_mat = keccak256(abi.encodePacked(ilk, issuanceTs, maturityTs)); // iss, mat
+        uint256 prevBalance = cfm.cBal(user_address, ilkClass_iss_mat);
+
+        try cfm.withdraw(ilk, user_address, issuanceTs, maturityTs, bal) {
+            assert(cfm.cBal(user_address, ilkClass_iss_mat) == prevBalance - bal);
+        }catch Error (string memory error_message) {
+            assert(
+                cfm.wards(msg.sender) == 0 && testUtil.cmpStr(error_message, "gate/not-authorized") ||
+                cfm.cBal(user_address, ilkClass_iss_mat) < bal && testUtil.cmpStr(error_message, "cBal/insufficient-balance")
+            );
+        } catch {
+            assert(false);
+        }
+    }
+
+    function rely(address usr_address) public {
+        try cfm.rely(usr_address) {
+            assert(cfm.wards(usr_address) == 1);
         } catch Error(string memory error_message) {
             assert(false);
         }
